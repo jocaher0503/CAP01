bc7e19c12d32d0a76ab452057e7a694c2f0eeccf
jocaher0503 <jocaher0503@g.educaand.es>
2023-10-04 12:48:18 +0200

    04/10/2023

--- CAP01/src/CAP01EJ02.java
+++ CAP01/src/CAP01EJ02.java
@@ -3,4 +3,5 @@ public class CAP01EJ02{
         System.out.println("Nombre: Juan Carlos Ocaña Heredia");
         System.out.println("Dirección: Calle Fragata 2");
         System.out.println("Teléfono: 622 715 570");
-    }}
+    }
+}
